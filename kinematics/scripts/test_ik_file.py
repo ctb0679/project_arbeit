36a8c2a46539a010fa8a92ed7fa20649ac2cf0a1
crc8643 <ayush.patel@tuhh.de>
2024-07-02 16:38:55 +0200

    IK fix

--- kinematics/scripts/test_ik_file.py
+++ kinematics/scripts/test_ik_file.py
@@ -12,16 +12,23 @@ def calculate_ik():
 
     pub = rospy.Publisher('/mycobot/angles_goal', MycobotSetAngles, queue_size=10)
     joint_angles = MycobotSetAngles()
-    rate = rospy.Rate(70)
+    rate = rospy.Rate(10)
 
     joint_offsets = [0, -np.pi/2, 0, -np.pi/2, np.pi/2, 0]
+    initial_positions = [0, 0, 0, 0, 0, 0]
 
     # Define the initial and desired joint angles
-    initial_msg = rospy.wait_for_message('/mycobot/angles_real', MycobotAngles, timeout=5)
-    initial_positions = initial_msg.position
+    initial_msg = rospy.wait_for_message('/mycobot/angles_real', MycobotAngles, timeout=5) 
+
+    initial_positions[0] = initial_msg.joint_1
+    initial_positions[1] = initial_msg.joint_2
+    initial_positions[2] = initial_msg.joint_3
+    initial_positions[3] = initial_msg.joint_4
+    initial_positions[4] = initial_msg.joint_5
+    initial_positions[5] = initial_msg.joint_6
 
     target_point = [0.2, -0.06, 0.23] #In metres
-    target_orientation = [1.3, -2.1, 3.0] #In degrees
+    target_orientation = [1.3, -2.1, 3.0] #In radians
     target_pose_matrix = calculate_transformation_matrix(target_point, target_orientation)
 
     print("Calculating joint states.....")
@@ -31,19 +38,17 @@ def calculate_ik():
     for i in range(len(target_js)):
         target_js[i] -= joint_offsets[i]
 
-    joint_angles.joint_1 = target_js[0]
-    joint_angles.joint_2 = target_js[1]
-    joint_angles.joint_3 = target_js[2]
-    joint_angles.joint_4 = target_js[3]
-    joint_angles.joint_5 = target_js[4]
-    joint_angles.joint_6 = target_js[5]
+    joint_angles.joint_1 = np.rad2deg(target_js[0])
+    joint_angles.joint_2 = np.rad2deg(target_js[1])
+    joint_angles.joint_3 = np.rad2deg(target_js[2])
+    joint_angles.joint_4 = np.rad2deg(target_js[3])
+    joint_angles.joint_5 = np.rad2deg(target_js[4])
+    joint_angles.joint_6 = np.rad2deg(target_js[5])
     joint_angles.speed = 50
     
     # Publish the JointState message
     pub.publish(joint_angles)
     rospy.loginfo(joint_angles)        
-    sequence += 1
-    rate.sleep()
 
    
 
